ebadcf531931be92e128f878486403c3239f1260
Serhii_Paliienko <sergiopalienko@gmail.com>
2025-09-13 10:32:41 +0200

    fix

---
+++ lib/api/clientApi.ts
@@ -0,0 +1,36 @@
+import { User } from "@/types/user";
+import { CreateTask, Task } from "@/types/task";
+import { nextServer } from "./api";
+
+export interface Credentials {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+export async function register(credentials: Credentials) {
+  const { data } = await nextServer.post<User>("/auth/register", credentials);
+  return data;
+}
+
+export async function login(credentials: Credentials) {
+  const { data } = await nextServer.post<User>("/auth/login", credentials);
+  return data;
+}
+interface SessionStatus {
+  success: boolean;
+}
+export const checkSession = async () => {
+  const { data } = await nextServer.get<SessionStatus>("/auth/session");
+  return data.success;
+};
+
+export const getMe = async () => {
+  const { data } = await nextServer.get<User>("/users/current");
+  return data;
+};
+
+export const createTask = async (newTask: CreateTask): Promise<Task> => {
+  const { data } = await nextServer.post<Task>("/tasks", newTask);
+  return data;
+};
