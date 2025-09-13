51f01a424df1b3eb5f84e340c13ddf00e7371abc
Viktor Medvid <darkissdark@gmail.com>
2025-09-13 23:24:44 +0200

    Implement POST method for diary entries and simplify emotion handling in form submission

--- app/api/diary/route.ts
+++ app/api/diary/route.ts
@@ -36,32 +36,32 @@ export async function GET(request: NextRequest) {
   }
 }
 
-// export async function POST(request: NextRequest) {
-//   try {
-//     const cookieStore = await cookies();
+export async function POST(request: NextRequest) {
+  try {
+    const cookieStore = await cookies();
 
-//     const body = await request.json();
+    const body = await request.json();
 
-//     const res = await api.post("/diary", body, {
-//       headers: {
-//         Cookie: cookieStore.toString(),
-//         "Content-Type": "application/json",
-//       },
-//     });
+    const res = await api.post("/diary", body, {
+      headers: {
+        Cookie: cookieStore.toString(),
+        "Content-Type": "application/json",
+      },
+    });
 
-//     return NextResponse.json(res.data, { status: res.status });
-//   } catch (error) {
-//     if (isAxiosError(error)) {
-//       logErrorResponse(error.response?.data);
-//       return NextResponse.json(
-//         { error: error.message, response: error.response?.data },
-//         { status: error.status }
-//       );
-//     }
-//     logErrorResponse({ message: (error as Error).message });
-//     return NextResponse.json(
-//       { error: "Internal Server Error" },
-//       { status: 500 }
-//     );
-//   }
-// }
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
