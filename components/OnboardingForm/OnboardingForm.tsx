984393055115dc2384a92638a6e0471b86253290
Alyona Lyalina <lyalina.alyona@icloud.com>
2025-09-15 19:41:46 +0300

    Add core logic to onboarding form and onboarding customer select

--- components/OnboardingForm/OnboardingForm.tsx
+++ components/OnboardingForm/OnboardingForm.tsx
@@ -1,25 +1,68 @@
 "use client";
 
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
 import * as Yup from "yup";
 import styles from "./OnboardingForm.module.css";
 import OnboardingAvatar from "../OnboardingAvatar/OnboardingAvatar";
+import OnbordingCustomSelect from "../OnbordingCustomSelect/OnbordingCustomSelect";
+import useAuthStore from "@/lib/store/authStore";
+import { updateUser } from "@/lib/api/clientApi";
+import { useState } from "react";
+import { ApiError } from "next/dist/server/api-utils";
+import { useRouter } from "next/navigation";
 
 interface OnboardingFormProps {
   onSubmit: (formData: FormData) => Promise<void>;
   isLoading: boolean;
 }
 
+interface OnborgindFormValues {
+  babyGender: string;
+  dueDate: string;
+}
+
+const today = new Date();
+today.setHours(0, 0, 0, 0);
+
+const minDate = new Date(today);
+minDate.setDate(today.getDate() + 7);
+
+const maxDate = new Date(today);
+maxDate.setDate(today.getDate() + 41 * 7);
+
 // --- Validation schema ---
 const validationSchema = Yup.object({
-  gender: Yup.string().required("Оберіть стать дитини"),
-  birthDate: Yup.string().required("Вкажіть дату пологів"),
+  babyGender: Yup.string().oneOf(
+    ["boy", "girl", "unknown"],
+    "invalid category"
+  ),
+  dueDate: Yup.date()
+    .min(minDate, "Дата має бути не раніше ніж через 1 тиждень")
+    .max(maxDate, "Дата має бути не пізніше ніж через 41 тиждень"),
 });
+const initialValues = { babyGender: "", dueDate: "" };
 
-export default function OnboardingForm({
-  onSubmit,
-  isLoading,
-}: OnboardingFormProps) {
+export default function OnboardingForm({ isLoading }: OnboardingFormProps) {
+  const user = useAuthStore((state) => state.user);
+  const setUser = useAuthStore((state) => state.setUser);
+  const [error, seterror] = useState("");
+  const router = useRouter();
+  const handleSubmit = async (
+    values: OnborgindFormValues,
+    actions: FormikHelpers<OnborgindFormValues>
+  ) => {
+    try {
+      const updatedUser = await updateUser(values);
+      setUser({
+        ...user,
+        ...updatedUser,
+      });
+      actions.resetForm();
+      router.push("/");
+    } catch (error) {
+      seterror((error as ApiError).message);
+    }
+  };
   return (
     <>
       {/* Page title */}
@@ -31,74 +74,62 @@ export default function OnboardingForm({
       <OnboardingAvatar />
 
       <Formik
-        initialValues={{ gender: "", birthDate: "" }}
+        initialValues={initialValues}
         validationSchema={validationSchema}
-        onSubmit={(values) => {
-          const formData = new FormData();
-          formData.append("gender", values.gender);
-          formData.append("birthDate", values.birthDate);
-          onSubmit(formData);
-        }}
+        onSubmit={handleSubmit}
+        validateOnChange
+        validateOnBlur
       >
-        {({ isSubmitting, values }) => (
+        {({ isSubmitting, isValid, dirty, values }) => (
           <Form className={styles.form}>
             <div className={styles.formOnboarding}>
               {/* --- Gender field --- */}
               <div className={styles.formField}>
-                <label htmlFor="gender" className={styles.labelGender}>
-                  Стать дитини
-                </label>
                 <div className={styles.selectWrapper}>
-                  <Field
-                    as="select"
-                    id="gender"
-                    name="gender"
-                    className={styles.selectGender}
-                  >
-                    <option value="" disabled hidden>
-                      Оберіть стать
-                    </option>
-                    <option value="boy">Хлопчик</option>
-                    <option value="girl">Дівчинка</option>
-                    <option value="unknown">Ще не знаю</option>
-                  </Field>
-                  <span className={styles.customArrow}></span>
+                  <OnbordingCustomSelect
+                    name="babyGender"
+                    label="Стать дитини"
+                    options={[
+                      { label: "Хлопчик", value: "boy" },
+                      { label: "Дівчинка", value: "girl" },
+                      { label: "Ще не знаю", value: "unknown" },
+                    ]}
+                    placeholder="Оберіть стать"
+                  />
                 </div>
-                <ErrorMessage
-                  name="gender"
-                  component="div"
-                  className={styles.error}
-                />
+                <ErrorMessage name="babyGender">
+                  {(msg) => <span className={styles.errorMessage}>{msg}</span>}
+                </ErrorMessage>
               </div>
 
               {/* --- Birth date field --- */}
               <div className={styles.formField}>
-                <label htmlFor="birthDate" className={styles.labelbirthDate}>
+                <label htmlFor="dueDate" className={styles.labelbirthDate}>
                   Планова дата пологів
                 </label>
                 <Field
-                  id="birthDate"
-                  name="birthDate"
+                  id="dueDate"
+                  name="dueDate"
                   type="date"
                   className={styles.input}
                 />
-                <ErrorMessage
-                  name="birthDate"
-                  component="div"
-                  className={styles.error}
-                />
+                <ErrorMessage name="dueDate">
+                  {(msg) => <span className={styles.errorMessage}>{msg}</span>}
+                </ErrorMessage>
               </div>
 
               {/* --- Submit button --- */}
               <button
                 type="submit"
                 className={styles.submitButton}
-                data-gender={values.gender}
-                disabled={!values.gender || isSubmitting || isLoading}
+                disabled={!isValid || !dirty}
+                data-gender={values.babyGender}
               >
                 {isSubmitting || isLoading ? "Збереження..." : "Зберегти"}
               </button>
             </div>
+
+            {error && <p className={styles.apiError}>{error}</p>}
           </Form>
         )}
       </Formik>
